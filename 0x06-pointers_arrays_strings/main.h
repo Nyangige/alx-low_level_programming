3edb249edd48ff1706a3cade88f006863411a49b
Nyangige <nyangigejohn@gmail.com>
2022-09-22 06:26:18 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,7 +19,7 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
+int main(void)
 
 int _strcmp(char *s1, char *s2);
 #endif
