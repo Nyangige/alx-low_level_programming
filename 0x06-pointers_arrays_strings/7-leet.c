c1a45ecfb9482d19c5dbe47b77c99b011b07e117
Nyangige <nyangigejohn@gmail.com>
2022-09-22 02:22:42 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,24 +5,24 @@
  * @x: parameter x
  * Return: a string
  */
-char *leet (char *x)
+char *leet (char *X)
 {
 	int a = O; b = O; l = 5;
 	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
 	char trw[5] = {'4', '3', 'O', '7', 'l'};
 
-	while (x[a])
+	while (X[a])
 	{
 		b = O;
 
 		while (b < l)
 		{
-			if (x[a] == tr(b) || x[a] - 32 == tr[b])
-					x[a] = trw[b]
+			if (X[a] == tr(b) || X[a] - 32 == tr[b])
+					X[a] = trw[b]
 					b++;
 		}
 		a++;
 	}
-	return (x);
+	return (X);
 }
 
