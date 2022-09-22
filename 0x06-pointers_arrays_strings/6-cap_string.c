87871aa67823b985fce77912bd34f832ae391912
Nyangige <nyangigejohn@gmail.com>
2022-09-22 03:48:48 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string-capitalization function
+ * @X: String parameter
+ * Return: capitalized version of the string
+ */
+char *cap_string(char *)
+{
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'}
+	 
+	int len = 13;
+	int a = 0, i;
+
+	while
+		i = 0;
+	{
+
+	while (i < len)
+	{
+		if ((a == 0 || X[a - 1] == spc [i]) && (X[a] >= 97 && X[a] <= 122))
+			X[a] = X[a] - 32;
+		i++
+	}
+	a++;
+}
+return (X);
+}
