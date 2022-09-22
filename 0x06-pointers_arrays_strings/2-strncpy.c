663c462277f2bc6aa5ecc27d5501f88db9821d9f
Nyangige <nyangigejohn@gmail.com>
2022-09-22 01:11:23 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,8 +6,9 @@
  * @dest: parameter one
  * @src: parameter 2
  * @n: parameter 3
+ * Return: String
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)\
 {
 	strncat (dest, src, n);
 	return (dest);
