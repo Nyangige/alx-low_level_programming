a8f87b58d6fc9aefa4e0b04a75aeb8ec3c814704
Nyangige <nyangigejohn@gmail.com>
2022-10-11 03:41:11 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,12 +13,12 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 /**
  * dog_t - Typedef for dog structure
  */
+int _putchar(char c);
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
