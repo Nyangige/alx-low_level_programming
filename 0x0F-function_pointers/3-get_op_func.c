ab4eb20d83fa505a7a9b99d6b75eb3be7fb541dc
Nyangige <nyangigejohn@gmail.com>
2022-10-15 13:10:02 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,95 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
 
-#include <stdlib.h>
-
-#include <string.h>
-
-
-
 /**
-
-<<<<<<< HEAD
-
- * get_op_func - ...
-
- * @s: ...
-
- *
-
- * Return: ...
-
+ * get_op_func - matches operator from main
+ * @s: op str
+ * Return: 0
  */
-
-=======
-
-  * get_op_func - ...
-
-  * @s: ...
-
-  *
-
-  * Return: ...
-
-  */
-
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
-
 int (*get_op_func(char *s))(int, int)
-
 {
-
-	op_t ops[] = {
-
-	{ "+", op_add },
-
-	{ "-", op_sub },
-
-	{ "*", op_mul },
-
-<<<<<<< HEAD
-
-	{ "/", op_div }
-
-=======
-
-	{ "/", op_div },
-
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
-
-	{ "%", op_mod },
-
-	{ NULL, NULL }
-
+	op_t op_s[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 
 	int i = 0;
 
-
-
-	while (i < 5)
-
+	while (op_s[i].op)
 	{
-
-		if (strcmp(s, ops[i].op) == 0)
-
-			return (ops[i].f);
-
-<<<<<<< HEAD
-
-		
-
-=======
-
-
-
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
-
+		if (*(op_s[i].op) == *s)
+			return (op_s[i].f);
 		i++;
-
 	}
-
-
-
-	return (0);
-
+	return (NULL);
 }
