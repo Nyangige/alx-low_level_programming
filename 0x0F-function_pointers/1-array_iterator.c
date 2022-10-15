2897a75626a648e6b5cc5cf334a3b0b99854fd34
Nyangige <nyangigejohn@gmail.com>
2022-10-15 12:37:02 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,20 +3,19 @@
 
 /**
  * array_iterator - iterates an array
- * @array-is an array
- * @size:-is the size
- * @action:is something
+ * @array: is an array
+ *  @size: is the size
+ *  @action: is something
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	if (array != NULL && action != NULL && size > 0)
-	{
-		while (i < size)
-		{
-			action(array[i]);
-			i++;
-		}
-	}
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
